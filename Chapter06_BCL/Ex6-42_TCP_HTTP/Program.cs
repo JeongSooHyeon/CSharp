daea9e237d59ac8de5494b2195ccce8d0cb242e8
SooHyeon Jeong <jsh75721@naver.com>
2023-07-13 01:14:19 +0900

    7.7 네트워크 통신 끝

---
+++ Chapter06_BCL/Ex6-42_TCP_HTTP/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using(Socket srvSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+        {
+            Console.WriteLine("http://localhost:8000 으로 방문해 보세요.");
+
+            IPEndPoint endPiont = new IPEndPoint(IPAddress.Any, 8000);
+
+            srvSocket.Bind(endPiont);
+            srvSocket.Listen(10);
+
+            while(true)
+            {
+                Socket clntSocket = srvSocket.Accept();
+                ThreadPool.QueueUserWorkItem(httpProcessFunc, clntSocket);
+            }
+        }
+    }
+
+    private static void httpProcessFunc(object state)
+    {
+        Socket socket = state as Socket;
+
+        // HTTP 프로토콘 통신에 따라 Send/Receive
+        byte[] reqBuf = new byte[4096];
+        socket.Receive(reqBuf);
+
+        string header = "HTTP/1.0 200 OK\nContent-Type: text/html; charset=UTF-8\r\n\r\n";
+        string body = "<html><body><mark>테스트 HTML</mark> 웹 페이지입니다.</body><html>";
+
+        byte[] respBuf = Encoding.UTF8.GetBytes(header + body);
+
+        socket.Send(respBuf);
+
+        socket.Close();
+    }
+}
---
+++ Chapter06_BCL/Unit6-7-7_WebClient/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 서버 소켓이 동작하는 스레드
+        Thread serverThread = new Thread(serverFunc);
+        serverThread.IsBackground = true;
+        serverThread.Start();
+        Thread.Sleep(500);  // 소케 서버용 스레드가 실행될 시간을 주기 위해
+
+        // 클라이언트 소켓이 동작하는 스레드
+        // 다중 UDP 클라이언트 실행
+        Thread clientThread = new Thread(clientFunc);
+        clientThread.IsBackground = true;
+        clientThread.Start();
+        Thread.Sleep(3000);
+
+        Console.WriteLine("종료하려면 아무 키나 누르세요...");
+        Console.ReadLine();
+    }
+
+    private static void serverFunc(object obj)
+    {
+        // [서버 소켓 코드 작성]
+        using (Socket srvSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 11200);
+
+            srvSocket.Bind(endPoint);
+            srvSocket.Listen(10);
+
+            while (true)
+            {
+                Socket clntSocket = srvSocket.Accept();
+
+                byte[] recvBytes = new byte[1024];
+
+                int nRecv = clntSocket.Receive(recvBytes);
+                string txt = Encoding.UTF8.GetString(recvBytes, 0, nRecv);
+
+                byte[] sendBytes = Encoding.UTF8.GetBytes("Hello : " + txt);
+                clntSocket.Send(sendBytes);
+                clntSocket.Close();
+            }
+        }
+    }
+    private static void clientFunc(object obj)
+    {
+        // WebClient 타입은 내부적으로 HttpWebRequest 객체를 사용해 통신
+        WebClient wc = new WebClient();
+        string responseText = wc.DownloadString("http://www.naver.com");
+
+
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        EndPoint serverEP = new IPEndPoint(IPAddress.Loopback, 11200);
+
+        socket.Connect(serverEP);
+
+        // HttpWebRequest 타입은 내부적으로 TCP 소켓을 생성하고
+        HttpWebRequest req = WebRequest.Create("http://www.naver.com") as HttpWebRequest;
+        // GetResponse 호출 단계에서 지정된 웹 서버로 HTTP 요청을 보내고 응답을 받는다.
+        HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
+
+        // 응답 내용을 담고 있는 Stream으로부터 문자열을 반환해서 출력한다.
+        using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+        {
+            string responseText = sr.ReadToEnd();
+            Console.WriteLine(responseText);
+            File.WriteAllText("naverpage.html", responseText);
+        }
+    }
+}
