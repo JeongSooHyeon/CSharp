a0252453515a02087635b6e8b20a37b328218f8e
SooHyeon Jeong <jsh75721@naver.com>
2023-08-21 17:22:30 +0900

    16장 C# 8.0 끝

---
+++ Chapter16_CSharp8.0/Unit16-12_Generic_Uunmanaged/Program.cs
@@ -0,0 +1,110 @@
+﻿
+
+using System.Runtime.InteropServices;
+
+public struct BlittableStruct
+{
+    public byte Item;
+}
+
+public struct BlittableGenericStruct<T>
+{
+    public T Item;
+}
+
+class Program
+{
+    unsafe static void Main()
+    {
+        // 컴파일 가능
+        {
+            BlittableStruct inst = new BlittableStruct();
+            BlittableStruct* pInst = &inst;
+        }
+
+        // BlittableStruct와 동일한 메모리의 구조임에도 이전에는 컴파일 오류
+        {
+            BlittableGenericStruct<byte> inst = new BlittableGenericStruct<byte>();
+            BlittableGenericStruct<byte>* pInst = &inst;
+        }
+    }
+}
+
+public class Nested<T> where T : unmanaged
+{
+    public T item;
+}
+class Program1
+{
+    unsafe static void Main()
+    {
+        // 컴파일 가능
+        Nested<byte> inst1 = new Nested<byte>();
+
+        // BlittableGenericStruct<byte> 타입 자체가 unmanaged 조건을 만족하지만, 이전에는 컴파일 오류
+        Nested<BlittableGenericStruct<byte>> inst2 = new Nested<BlittableGenericStruct<byte>();
+
+        // 이전에는 컴파일 오류
+        static unsafe void CallUnmanaged<T>(T inst) where T : unmanaged
+        {
+            T* pInst = &inst;
+        }
+    }
+}
+
+public unsafe ref struct NativeMemory<T> where T : unmanaged
+{
+    int _size;
+    IntPtr _ptr;
+
+    public NativeMemory(int size)
+    {
+        _size = size;
+        _ptr = Marshal.AllocHGlobal(size * sizeof(T));
+    }
+
+    public Span<T> GetView()
+    {
+        return new Span<T>(_ptr.ToPointer(), _size);
+    }
+
+    public void Dispose()
+    {
+        if(_ptr == IntPtr.Zero)
+        {
+            return;
+        }
+
+        Marshal.FreeHGlobal(_ptr);
+        _ptr = IntPtr.Zero;
+    }
+}
+
+class Program3
+{
+    static unsafe void Main() {
+        // 아래 코드는 아무리 실행해도 GC가 발생하지 않음
+        while (true)
+        {
+            // 비-관리 메모리로부터 int[1024] 공간만큼 할당받아 사용
+            using (NativeMemory<int> buf = new NativeMemory<int>(1024))
+            {
+                Span<int> viewBuf = buf.GetView();
+                for (int i = 0; i < viewBuf.Length; i++)
+                {
+                    viewBuf[i] = 0;
+                }
+            }
+        }
+
+        // 비-관리 메모리로부터 byte[1024] 공간만큼 할당받아 사용
+        using (NativeMemory<byte> buf = new NativeMemory<byte>(1024))
+        {
+            Span<byte> viewBuf = buf.GetView();
+            for (int i = 0; i < viewBuf.Length; i++)
+            {
+                viewBuf[i] = (byte)i;
+            }
+        }
+    }
+}
---
+++ Chapter16_CSharp8.0/Unit16-13_Struct_Readonly/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public struct Vector2
+{
+    public float x;
+    public float y;
+
+    public (float x, float y) ToTuple()
+    {
+        return (x, y);
+    }
+
+    // readonly 예약어 적용
+    public readonly(float x, float y) ToTuple1()
+    {
+        return (x, y);
+    }
+
+    // 속성 property
+    public readonly float LengthSquared
+    {
+        get { return (x * x) + (y * y); }
+    }
+
+    // 자동 구현 속성의 경우 get에 대해서만 readonly 적용 가능
+    public float Z { readonly get; set; }
+
+    // 람다 식으로 정의한 메서드
+    public readonly float GetLength => (x * x) + (y * y);
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vector2 v = new Vector2 { x = 5, y = 6 };
+        OutputInfo(v);
+    }
+
+    static void OutputInfo(in Vector2 v2)
+    {
+        (float x, float y) = v2.ToTuple(); // 방어 복사본 생성
+        Console.WriteLine($"({x},{y})");
+    }
+}
